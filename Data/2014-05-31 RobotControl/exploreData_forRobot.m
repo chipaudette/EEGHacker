e5956ede9ee1a6f05750fcc286eb9c96f4bb4215
Chip Audette <chipaudette@yahoo.com>
2014-06-16 12:16:17 -0400

    Data: expand analysis of hexbug data

--- Data/2014-05-31 RobotControl/exploreData_forRobot.m
+++ Data/2014-05-31 RobotControl/exploreData_forRobot.m
@@ -4,9 +4,22 @@
 f_lim = [0 22];
 t_lim=[];
 pname = 'SavedData\';
-
-%fname = 'openBCI_raw_2014-05-08_21-24-31_countbackby3_countbackby41from1200.txt';chans=[1:3];f_lim = [0 100];
-fname = 'openBCI_raw_2014-05-31_20-57-51_RobotControl2.txt'; chans=[2];t_lim = [10 140];
+switch 5
+    case 2
+        fname = 'openBCI_raw_2014-05-31_20-48-01_Robot02.txt'; chans=[2];
+    case 3
+        fname = 'openBCI_raw_2014-05-31_20-51-30_Robot03.txt'; chans=[2];
+    case 4
+        fname = 'openBCI_raw_2014-05-31_20-55-29_Robot04.txt'; chans=[2];
+    case 5
+        fname = 'openBCI_raw_2014-05-31_20-57-51_Robot05.txt'; chans=2;  t_lim=[0 135];%this might be the one from the movie
+    case 9
+        fname = 'openBCI_raw_2014-05-31_21-07-40_Robot09.txt'; chans=2;
+    case 11
+        fname = 'openBCI_raw_2014-05-31_21-15-57_Robot11.txt'; chans = 2;  %alpha, then 7.5 Hz sustained
+    case 12
+        fname = 'openBCI_raw_2014-05-31_21-17-28_Robot12.txt'; chans = 2;
+end
 scale_fac_volts_count=2.23e-8;
 
 
@@ -48,38 +61,40 @@ data_V = filter(b,a,data_V);
 
 %% plot data
 t_sec = ([1:size(data_V,1)]-1)/fs;
-nrow = 2; ncol=1;
+nrow = 3; ncol=1;
 ax=[];
-figure;setFigureTallestWidest;
+figure;setFigureTallestWide;
 for Ichan=1:1
-
-%     %time-domain plot
-%     subplotTightBorder(nrow,ncol,(Ichan-1)*2+1);
-%     plot(t_sec,data_V(:,Ichan)*1e6);
-%     xlim(t_sec([1 end]));
-%     %ylim(1e6*(median_data_V(Ichan)+3*[-1 1]*spread_data_V(Ichan)));
-%     ylim([-200 200]);
-%     weaText({['Mean = ' num2str(mean(data_V(:,Ichan))*1e6,3) ' uV'];
-%         ['Std = ' num2str(std(data_V(:,Ichan))*1e6,3) ' uV']},2);
-%     title(['Channel ' num2str(Ichan)]);
-%     xlabel(['Time (sec)']);
-%     ylabel(['Signal (uV)']);
-%     ax(end+1)=gca;
-
+    
+    %     %time-domain plot
+    %     subplotTightBorder(nrow,ncol,(Ichan-1)*2+1);
+    %     plot(t_sec,data_V(:,Ichan)*1e6);
+    %     xlim(t_sec([1 end]));
+    %     %ylim(1e6*(median_data_V(Ichan)+3*[-1 1]*spread_data_V(Ichan)));
+    %     ylim([-200 200]);
+    %     weaText({['Mean = ' num2str(mean(data_V(:,Ichan))*1e6,3) ' uV'];
+    %         ['Std = ' num2str(std(data_V(:,Ichan))*1e6,3) ' uV']},2);
+    %     title(['Channel ' num2str(Ichan)]);
+    %     xlabel(['Time (sec)']);
+    %     ylabel(['Signal (uV)']);
+    %     ax(end+1)=gca;
+    
     %spectrogram
-    subplotTightBorder(nrow,ncol,Ichan);
+    subplot(nrow,ncol,Ichan);
     %N=1024;
     %N=1200;overlap = 1-1/32;plots=0;
     %N = 2400;overlap = 1-1/64;plots=0; yl=[0 15];
     %N=512;overlap = 1-1/16;plots=0;
-     N=512;overlap = 1-50/N;plots=0;  %this is the overlap in the processing GUI
+    N=512;overlap = 1-50/N;plots=0;  %this is the overlap in the processing GUI
     [pD,wT,f]=windowedFFTPlot_spectragram(data_V(:,Ichan)*1e6,N,overlap,fs,plots);
     wT = wT + (N/2)/fs;
     
     %FFT Averaging (in dB space)
+    smooth_txt=[];
     if (1)
         pD_dB = 10*log10(pD);
         smooth_fac = 0.9;
+        smooth_txt = ['Smooth Fac: ' num2str(smooth_fac)];
         b = 1-smooth_fac; a = [1 -smooth_fac];
         pD_dB = filter(b,a,pD_dB')';  %transpose to smooth across columns
         pD = 10.^(0.1*pD_dB);
@@ -90,8 +105,9 @@ for Ichan=1:1
     set(gca,'Ydir','normal');
     xlabel('Time (sec)');
     ylabel('Frequency (Hz)');
-    title([fname ', Channel ' num2str(Ichan)],'interpreter','none');
-    set(gca,'Clim',+25+[-40 0]+10*log10(256)-10*log10(N));
+    title([fname ', Channel ' num2str(chans(Ichan))],'interpreter','none');
+    %set(gca,'Clim',+25+[-40 0]+10*log10(256)-10*log10(N));
+    set(gca,'Clim',[-15 15]);
     if ~isempty(t_lim)
         xlim(t_lim);
     else
@@ -100,9 +116,18 @@ for Ichan=1:1
     xl=xlim;
     ylim(f_lim);
     cl=get(gca,'Clim');
-    h=weaText({['Nfft = ' num2str(N) ', fs = ' num2str(fs) ' Hz'];['Clim = [' num2str(round(cl(1))) ' ' num2str(round(cl(2))) '] dB']},1);
+    txt = {['fs: ' num2str(fs) ' Hz, N: ' num2str(N) ', Step: ' num2str(round(N*(1-overlap)))];['Clim = [' num2str(round(cl(1))) ' ' num2str(round(cl(2))) '] dB']};
+    if ~isempty(smooth_txt); txt{end+1}=smooth_txt;end
+    h=weaText(txt,1);
     set(h,'BackgroundColor','white');
     colorbar;
+    clabel(['uV/sqrt(Hz) (dB)']);
+    
+%     hold on;
+%     plot(xlim,inband_Hz(1)*[1 1],'w--','linewidth',2);
+%     plot(xlim,inband_Hz(2)*[1 1],'w--','linewidth',2);
+%     hold off
+    
     ax(end+1)=gca;
     
     %    %compute SNR
@@ -127,38 +152,51 @@ for Ichan=1:1
     t_snr_sec = wT;
     
     %continue plotting
-    subplotTightBorder(nrow,ncol,Ichan+1);
-    imagesc(wT,f,snr_dB);
-    set(gca,'Ydir','normal');
-    xlabel('Time (sec)');
-    ylabel('Frequency (Hz)');
-    title([fname ', Channel ' num2str(Ichan)],'interpreter','none');
-    set(gca,'Clim',[-10 10]);
-    xlim(xl);
-    ylim(f_lim);
-    cl=get(gca,'Clim');
-    h=weaText({['Nfft = ' num2str(N) ', fs = ' num2str(fs) ' Hz'];['Clim = [' num2str(round(cl(1))) ' ' num2str(round(cl(2))) '] dB']},1);
-    set(h,'BackgroundColor','white');
-    
-    det_thresh_dB = 6;
-    I=find(peak_SNR_dB > det_thresh_dB);
-    hold on; plot(t_snr_sec(I),peak_freq_Hz(I),'wo','linewidth',2); hold off;
-    
-    freq_bounds = [4 6.5 9 12 15];
-    for Ibound=1:length(freq_bounds);
-        hold on;
-        plot(xlim,freq_bounds(Ibound)*[1 1],'w:');
-        hold off;
+    for Iplot=1:2
+        subplot(nrow,ncol,Ichan+Iplot);
+%         if Iplot==1
+            foo_dB = snr_dB;
+%         else
+%             foo_dB = snr_dB;
+%             I = find(foo_dB<det_thresh_dB);
+%             foo_dB(I) = -10;
+%             I = find((f < inband_Hz(1)) | (f > inband_Hz(2)));
+%             foo_dB(I,:) = -10;
+%             %I = find(foo>=det_thresh_dB)
+%             %foo(I)
+%         end
+        imagesc(wT,f,foo_dB);
+        set(gca,'Ydir','normal');
+        xlabel('Time (sec)');
+        ylabel('Frequency (Hz)');
+        title([fname ', Channel ' num2str(chans(Ichan))],'interpreter','none');
+        set(gca,'Clim',[-10 10]);
+        set(gca,'Clim',[-5 10]);
+        xlim(xl);
+        ylim(f_lim);
+        cl=get(gca,'Clim');
+        %h=weaText({['Nfft = ' num2str(N) ', fs = ' num2str(fs) ' Hz'];['Clim = [' num2str(round(cl(1))) ' ' num2str(round(cl(2))) '] dB']},1);
+        h = weaText(txt,1);
+        set(h,'BackgroundColor','white');
+        clabel(['SNR (dB)']);
+
+        
+        det_thresh_dB = 6;
+        I=find(peak_SNR_dB > det_thresh_dB);
+        if (Iplot==2)
+            hold on; plot(t_snr_sec(I),peak_freq_Hz(I),'wo','linewidth',2); hold off;
+           
+            %freq_bounds = [4 6.5 9 12 15];
+            freq_bounds = [4 6.5 9 12];
+            for Ibound=1:length(freq_bounds);
+                hold on;
+                plot(xlim,freq_bounds(Ibound)*[1 1],'w--','linewidth',2);
+                hold off;
+            end
+ 
+        end
+        ax(end+1)=gca;
     end
-    
-%     hold on;
-%     plot(xlim,inband_Hz(1)*[1 1],'w--','linewidth',2);
-%     plot(xlim,inband_Hz(2)*[1 1],'w--','linewidth',2);
-%     hold off
-    colorbar;
-    ax(end+1)=gca;
-
-    
-end
-
+end    
 linkaxes(ax);
+
